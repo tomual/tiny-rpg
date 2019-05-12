8e7161d697e88b67d66a8961efc4d170d157b85d
tomual <tomual@users.noreply.github.com>
2019-05-12 16:44:43 +0800

    Player move

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float distanceToMove;
+    [SerializeField] private float moveSpeed;
+
+    private bool moveToPoint = false;
+    private Vector3 endPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        endPosition = transform.position;
     }
 
-    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (moveToPoint)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, endPosition, moveSpeed * Time.deltaTime);
+            Debug.Log(Vector3.Distance(transform.position, endPosition));
+            if (Vector3.Distance(transform.position, endPosition) == 0)
+            {
+                moveToPoint = false;
+            }
+        }
+    }
+
     void Update()
     {
+        if (!moveToPoint)
+        {
+            if (Input.GetKey(KeyCode.A)) //Left
+            {
+                endPosition = new Vector3(endPosition.x - distanceToMove, endPosition.y, endPosition.z);
+                moveToPoint = true;
+            }
+            if (Input.GetKey(KeyCode.D)) //Right
+            {
+                endPosition = new Vector3(endPosition.x + distanceToMove, endPosition.y, endPosition.z);
+                moveToPoint = true;
+            }
+            if (Input.GetKey(KeyCode.W)) //Up
+            {
+                endPosition = new Vector3(endPosition.x, endPosition.y + distanceToMove, endPosition.z);
+                moveToPoint = true;
+            }
+            if (Input.GetKey(KeyCode.S)) //Down
+            {
+                endPosition = new Vector3(endPosition.x, endPosition.y - distanceToMove, endPosition.z);
+                moveToPoint = true;
+            }
+        }
         
+
     }
 }
