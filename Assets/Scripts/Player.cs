7308fbd7789b0fab7181b5d6c50209890a3b6974
tomual <tomual@users.noreply.github.com>
2019-05-13 15:41:48 +0800

    Detect teleport type collider

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,10 +11,13 @@ public class Player : MonoBehaviour
     private Vector3 endPosition;
     private Vector3 startRayPosition;
 
+    GameObject blackScreen;
+
     // Start is called before the first frame update
     void Start()
     {
         endPosition = transform.position;
+        blackScreen = GameObject.FindGameObjectWithTag("BlackScreen");
     }
 
     void FixedUpdate()
@@ -37,8 +40,7 @@ public class Player : MonoBehaviour
             if (Input.GetKey(KeyCode.A))
             {
                 startRayPosition = new Vector3(transform.position.x - distanceToMove, transform.position.y, transform.position.z);
-                RaycastHit2D hit = Physics2D.Raycast(startRayPosition, Vector2.left, 0.1f);
-                if (hit.collider == null)
+                if (CanWalkAhead(startRayPosition))
                 {
                     endPosition = new Vector3(endPosition.x - distanceToMove, endPosition.y, endPosition.z);
                     moveToPoint = true;
@@ -48,8 +50,7 @@ public class Player : MonoBehaviour
             if (Input.GetKey(KeyCode.D))
             {
                 startRayPosition = new Vector3(transform.position.x + distanceToMove, transform.position.y, transform.position.z);
-                RaycastHit2D hit = Physics2D.Raycast(startRayPosition, Vector2.left, 0.1f);
-                if (hit.collider == null)
+                if (CanWalkAhead(startRayPosition))
                 {
                     endPosition = new Vector3(endPosition.x + distanceToMove, endPosition.y, endPosition.z);
                     moveToPoint = true;
@@ -59,8 +60,7 @@ public class Player : MonoBehaviour
             if (Input.GetKey(KeyCode.W))
             {
                 startRayPosition = new Vector3(transform.position.x, transform.position.y + distanceToMove, transform.position.z);
-                RaycastHit2D hit = Physics2D.Raycast(startRayPosition, Vector2.left, 0.1f);
-                if (hit.collider == null)
+                if (CanWalkAhead(startRayPosition))
                 {
                     endPosition = new Vector3(endPosition.x, endPosition.y + distanceToMove, endPosition.z);
                     moveToPoint = true;
@@ -70,8 +70,7 @@ public class Player : MonoBehaviour
             if (Input.GetKey(KeyCode.S))
             {
                 startRayPosition = new Vector3(transform.position.x, transform.position.y - distanceToMove, transform.position.z);
-                RaycastHit2D hit = Physics2D.Raycast(startRayPosition, Vector2.left, 0.1f);
-                if (hit.collider == null)
+                if (CanWalkAhead(startRayPosition))
                 {
                     endPosition = new Vector3(endPosition.x, endPosition.y - distanceToMove, endPosition.z);
                     moveToPoint = true;
@@ -81,5 +80,32 @@ public class Player : MonoBehaviour
         }
     }
 
+    bool CanWalkAhead(Vector3 startRayPosition)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(startRayPosition, Vector2.left, 0.1f);
+        if (hit.collider != null)
+        {
+            if (!hit.collider.isTrigger)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision);
+        if (collision.gameObject.tag == "Teleporter")
+        {
+            Debug.Log("Teleporter");
+            blackScreen.GetComponentInChildren<CanvasGroup>().alpha = 1;
+        }
+    }
 
 }
